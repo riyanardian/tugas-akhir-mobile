21a7438aba9b0da48ee433190de33cfc120ba1e5
AlfanArafinda <arafindaalfan@gmail.com>
2024-12-24 23:12:14 +0700

    lihatdata

--- app/src/main/java/com/example/projectmobile/Recipe.kt
+++ app/src/main/java/com/example/projectmobile/Recipe.kt
@@ -2,8 +2,8 @@ package com.example.projectmobile
 
 data class Recipe(
         val id:String = "",
-        val title: String = "",        // Nama resep
-        val description: String = "",  // Deskripsi resep
-        val imageUrl: String = "",     // URL gambar resep
-        val timestamp: Long = 0        // Timestamp untuk urutan resep
-)
+        val title: String = "",
+        val description: String = "",
+        val imageUrl: String = "",
+        val timestamp: Long = 0L
+): Serializable
